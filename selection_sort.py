615b9b93bfba4f0bee5b7cc3a0df1d3a081f6458
soronto3603 <soronto3603@gmail.com>
2018-08-01 11:53:34 +0900

    complete

--- selection_sort.py
+++ selection_sort.py
@@ -1,12 +1,20 @@
+from PIL import Image
 def insertionSort(alist):
-    for index in range(1,len(alist)):
-        currentvalue = alist[index]
-        position = index
-        while position>0 and alist[position-1]>currentvalue:
-            alist[position]=alist[position-1]
-            position = position-1
-    alist[position]=currentvalue
-    return alist
+    b=alist
+    a=len(b)
+    im=Image.new('RGB',(a,a))
+    px=im.load()
+    
+    for i in range(a-1):
+        for j in range(i+1,a-1):
+            px[i,j]=(76,175,80)
+            if b[i] < b[j] :
+                px[i,j]=(244,67,54)
+                b[i],b[j]=b[j],b[i]
+    im.save("1.png")
+    return b
 
-
-
+import random
+c=[i for i in range(100000)]
+d=random.sample(c,20)
+insertionSort(d)
